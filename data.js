7a861beedca20d698acc9a82787bf139ef0766ab
slawekpl81 <slawomir.jona@gmail.com>
2024-06-20 20:27:54 +0200

    start

---
+++ data.js
@@ -0,0 +1,61 @@
+localizations = [
+  {
+    name: "nocleg",
+    town: "KazimierzDolny",
+    coord: [51.31526320761561, 21.953262725983183],
+    color: "blue",
+    popup_text: "Willa IMA Nasz nocleg!",
+    link: "https://www.booking.com/hotel/pl/willa-ima-kazimierz-dolny.pl.html",
+  },
+  {
+    name: "rynek",
+    town: "KazimierzDolny",
+    coord: [51.322192979414744, 21.947537997173526],
+    color: "green",
+    popup_text: "RYNEK",
+    link: "https://www.bing.com/images/search?q=kazimierz+dolny+rynek&form=HDRSC3&first=1",
+  },
+  {
+    name: "gora",
+    town: "KazimierzDolny",
+    coord: [51.32213400681986, 21.950581651814435],
+    color: "green",
+    popup_text: "Góra Trzech Krzyży",
+    link: "https://kazimierzdolny24.pl/warto-zobaczyc/punkty-widokowe/gora-trzech-krzyzy/",
+  },
+  {
+    name: "krzyztopor",
+    town: "Ujazd",
+    coord: [50.71420713945118, 21.310519841316786],
+    color: "green",
+    popup_text: "Zamek Krzyżtopór",
+    link: "https://krzyztopor.org.pl/zamek/index.php/pl/",
+  },
+  {
+    name: "rynek",
+    town: "Lublin",
+    coord: [51.24824817702742, 22.569529259308688],
+    color: "green",
+    popup_text: "RYNEK",
+    link: "https://www.podrozepoeuropie.pl/stare-miasto-lublin/",
+  },
+  {
+    name: "rynek",
+    town: "Sandomierz",
+    coord: [50.679516163700676, 21.749610622706967],
+    color: "green",
+    popup_text: "RYNEK",
+    link: "https://idziemydalej.pl/sandomierz-subiektywny-przewodnik/",
+  },
+  {
+    name: "gory_pieprzowe",
+    town: "Sandomierz",
+    coord: [50.6855546241928, 21.786246466721984],
+    color: "green",
+    popup_text: "Góry pieprzowe",
+    link: "https://idziemydalej.pl/sandomierz-subiektywny-przewodnik/",
+  },
+];
+
+// https://spatialgalaxy.net/categories/leaflet/
+// https://www.tutorialspoint.com/leafletjs/index.html
